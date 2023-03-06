81e0e220276c9bde11b2a13798016f5c1bed3a31
Prathamesh1811 <98310849+Prathamesh1811@users.noreply.github.com>
2023-03-06 08:44:57 +0000

    Added routes

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
+# from flask import Flask, request
+# import postTranslation
+# import IR
+
+# app = Flask(__name__)
+
+# app.add_url_rule('/azureTranslation', view_func=postTranslation.azure.translate_text)
+# app.add_url_rule('/multipleTranslation', view_func=postTranslation.multipleTranslation.translateText)
+# app.add_url_rule('/news', view_func=IR.news.google_search)
+# app.add_url_rule('/searchImage', view_func=IR.linkImage.retrieve_search_results)
+
+
+# if __name__ == '__main__':
+#    app.run()
+
 from flask import Flask, request
-import postTranslation
-import IR
+from postTranslation.azure import translate_text
+from postTranslation.multipleTranslation import translateText
+from IR.news import google_search
+from IR.linkImage import retrieve_search_results
+import hello
 
 app = Flask(__name__)
 
-app.add_url_rule('/azureTranslation', view_func=postTranslation.azure.translate_text)
-app.add_url_rule('/multipleTranslation', view_func=postTranslation.multipleTranslation.translateText)
-app.add_url_rule('/news', view_func=IR.news.google_search)
-app.add_url_rule('/searchImage', view_func=IR.linkImage.retrieve_search_results)
-
+app.add_url_rule('/azureTranslation', view_func=translate_text)
+# app.add_url_rule('/multipleTranslation', view_func=translateText)
+app.add_url_rule('/news', view_func=google_search)
+app.add_url_rule('/searchImage', view_func=retrieve_search_results)
+app.add_url_rule('/', view_func=hello)
 
 if __name__ == '__main__':
    app.run()
