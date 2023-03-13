8ca7c2968cd046e571bd8f20997b1377f40737eb
Prathamesh Anil Kenjale <prathameshkenjale11@gmail.com>
2023-03-13 17:53:41 +0530

    Changes func name in app.py file

--- IR/linkImage.py
+++ IR/linkImage.py
@@ -8,32 +8,42 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Setting user-agent header to avoid detection as a bot
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-}
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+# }
 
-def retrieve_search_results(url):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    search_results = soup.select("div.g")
-    results = []
-    for result in search_results:
-        title = result.select_one("h3").get_text()
-        link = result.select_one("a")["href"]
-        image = result.select_one("img")
-        if image:
-            image_url = image.get("src")
-        else:
-            image_url = None
-        results.append({"title": title, "link": link, "image_url": image_url})
-    return results
-
-@app.route("/news")
+@app.route("/searchImage")
 def get_news():
     query = request.args.get("q","")
     if not query:
         return jsonify({"error": "No query provided"})
     base_url = "https://www.google.com/search?q={}&num=10"
+
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
+    def retrieve_search_results(url):
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        search_results = soup.select("div.g")
+        results = []
+        for result in search_results:
+            title = result.select_one("h3").get_text()
+            link = result.select_one("a")["href"]
+            image = result.select_one("img")
+            if image:
+                image_url = image.get("src")
+            else:
+                image_url = None
+            results.append({"title": title, "link": link, "image_url": image_url})
+        return results
+
+# @app.route("/searchImage")
+# def get_news():
+#     query = request.args.get("q","")
+#     if not query:
+#         return jsonify({"error": "No query provided"})
+#     base_url = "https://www.google.com/search?q={}&num=10"
     with ThreadPoolExecutor(max_workers=5) as executor:
         urls = [base_url.format(query) + "&start=" + str(i) for i in range(1, 51, 10)]
         results = executor.map(retrieve_search_results, urls)
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from postTranslation.azure import translate_text
 from postTranslation.multipleTranslation import multipleTranslation
 from IR.news import google_search
-from IR.linkImage import retrieve_search_results
+from IR.linkImage import get_news
 import hello
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.add_url_rule('/azureTranslation', view_func=translate_text)
 app.add_url_rule('/multipleTranslation', view_func=multipleTranslation)
 app.add_url_rule('/news', view_func=google_search)
-app.add_url_rule('/searchImage', view_func=retrieve_search_results)
+app.add_url_rule('/searchImage', view_func=get_news)
 app.add_url_rule('/', view_func=hello.hello)
 
 if __name__ == '__main__':
