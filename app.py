6d69b2f03b6860d704df525f7ef3ae9fd1e03613
Prathamesh Anil Kenjale <prathameshkenjale11@gmail.com>
2023-03-13 10:21:25 +0530

    Added dockerfile

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-# from flask import Flask, request
-# import postTranslation
-# import IR
-
-# app = Flask(__name__)
-
-# app.add_url_rule('/azureTranslation', view_func=postTranslation.azure.translate_text)
-# app.add_url_rule('/multipleTranslation', view_func=postTranslation.multipleTranslation.translateText)
-# app.add_url_rule('/news', view_func=IR.news.google_search)
-# app.add_url_rule('/searchImage', view_func=IR.linkImage.retrieve_search_results)
-
-
-# if __name__ == '__main__':
-#    app.run()
 
 from flask import Flask, request
 from postTranslation.azure import translate_text
