ae6ae377fd43707dff658c31da016c368780189f
Prathamesh Anil Kenjale <prathameshkenjale11@gmail.com>
2023-05-31 16:15:19 +0530

    Added exceptions

--- postTranslation/translate.py
+++ postTranslation/translate.py
@@ -56,13 +56,34 @@ def translate(text, dest_languages):
 
 
 # Flask route to translate text
+# @app.route('/translate', methods=['POST'])
+# def translate_text():
+#     data = request.json
+#     text = data['text']
+#     dest_languages = data['dest_languages'].split(',')
+#     translations = translate(text, dest_languages)
+#     return jsonify(translations)
+
+# Flask route to translate text
 @app.route('/translate', methods=['POST'])
 def translate_text():
-    data = request.json
-    text = data['text']
-    dest_languages = data['dest_languages'].split(',')
-    translations = translate(text, dest_languages)
-    return jsonify(translations)
+    try:
+        data = request.json
+        if 'text' in data:
+            text = data['text']
+        else:
+            return jsonify({'error': 'Missing text parameter'}), 400
+
+        if 'dest_languages' in data:
+            dest_languages = data['dest_languages'].split(',')
+        else:
+            return jsonify({'error': 'Missing dest_languages parameter'}), 400
+
+        translations = translate(text, dest_languages)
+        return jsonify(translations)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
